08f64738edc85c144a3b4093128ea37eba47c710
Luis Angel <angel.rh.2108@gmail.com>
2022-12-03 13:09:15 -0600

    Agregar SASS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <button class="btn btn-success">success</button>
+    <button class="btn btn-primary">primary</button>
+    <button class="btn btn-danger">danger</button>
   </div>
+  
 </template>
