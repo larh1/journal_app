1d65b59bfafeafae6e73a3d8546f573d152bff1a
Luis Angel <angel.rh.2108@gmail.com>
2022-12-06 11:57:08 -0600

    Obtener Registros desde Getters

--- src/modules/daybook/components/ListaRegistros.vue
+++ src/modules/daybook/components/ListaRegistros.vue
@@ -2,34 +2,62 @@
 <div class="entry-list_container">
     <!-- Caja de busqueda -->
     <div class="py-2 px-2">
-        <input type="text" class="form-control" placeholder="Buscar...">
+        <input type="text" class="form-control" v-model="term" placeholder="Buscar...">
     </div>
     <div class="scroll-list">
-        <h4 :key="i" v-for="(r,i) in 100"> <Registro/></h4>
+        <h4 :key="i" v-for="(r,i) in entriesByTerm">
+            <Registro />
+        </h4>
     </div>
 </div>
 </template>
 
 <script>
-import { defineAsyncComponent } from 'vue'
+import
+{
+    defineAsyncComponent
+}
+from 'vue'
+import
+{
+    mapGetters
+}
+from 'vuex';
 export default
 {
     name: "registro-component",
     components:
     {
-        Registro:defineAsyncComponent(()=>import("./Registro.vue"))
-    }
+        Registro: defineAsyncComponent(() => import("./Registro.vue"))
+    },
+    computed:
+    {
+        // Modulo, nombre getter
+        ...mapGetters("journal", ["getEntriesByTerm"]),
+        entriesByTerm()
+        {
+            // Buscar los
+            return this.getEntriesByTerm(this.term);
+        }
+    },
+    data()
+    {
+        return {
+            term: ""
+        }
+    },
+    mounted()
+    {}
 }
 </script>
 
 <style lang="scss" scoped>
-
-.entry-list_container{
-    border-right:1px solid gray;
+.entry-list_container {
+    border-right: 1px solid gray;
     height: calc(100vh - 50px);
 }
-.scroll-list
-{
+
+.scroll-list {
     overflow: scroll;
     height: calc(100vh - 120px);
 }
--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -1,9 +1,17 @@
-export const getEntriesByTerm = () =>
-{
 
+/**
+ * Obtener los registros que coincidan con el termino ingresado
+ * @param  state State del Store
+ * @returns Array
+ */
+export const getEntriesByTerm = (state) => (term = "") =>
+{
+    if (term.trim().length == 0) return state.entries;
+
+    return state.entries.filter(e => e.text.toLowerCase().includes(term));
 }
 
 export const getEntriesById = () =>
 {
-    
+
 }
