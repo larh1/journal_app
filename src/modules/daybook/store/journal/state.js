c5fc84499824c8cc2991a55d523d4a0839646cc6
Luis Angel <angel.rh.2108@gmail.com>
2022-12-06 11:29:37 -0600

    Creacion de data ficticia

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,5 +1,31 @@
 export default () => (
     {
         isLoading: true,
-        entries: [1,2,3,4,5]
+        entries: [
+            {
+                id: 1,
+                date: new Date().toDateString(),
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                picture: null,
+            },
+            {
+                id: 2,
+                date: new Date().toDateString(),
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                picture: null,
+            },
+            {
+                id: 3,
+                date: new Date().toDateString(),
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                picture: null,
+            },
+            {
+                id: 4,
+                date: new Date().toDateString(),
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                picture: null,
+            },
+
+        ]
     })
