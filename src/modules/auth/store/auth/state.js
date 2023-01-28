29724b5d41e66d3c4e4fcf282016db08aad4fab1
Luis Angel <angel.rh.2108@gmail.com>
2023-01-28 11:33:50 -0600

    Auth - Creacino del modulo Vuex

---
+++ src/modules/auth/store/auth/state.js
@@ -0,0 +1,12 @@
+export default () => (
+    {
+        /**
+         * 1. Autenticando
+         * 2. Autenticado
+         * 1. No Autenticado
+         */
+        status: 0,
+        user: null,
+        idToken: null,
+        refreshToken: null,
+    })
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
+import auth from "@/modules/auth/store/auth";
 import journal from "@/modules/daybook/store/journal";
-import { createStore } from "vuex";
-
-
+import {createStore} from "vuex";
 
 const store = createStore({
     modules:
     {
         journal,
+        auth,
     }
 });
 
